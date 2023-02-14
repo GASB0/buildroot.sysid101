149b8ac48463b2a1e60ed5703ec6c8cd6ba5e23d
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2023-02-14 17:21:58 -0400

    Adding sysid101 and testPackage

---
+++ package/sysid101/sysid101.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# sysid package
+#
+################################################################################
+
+SYSID101_VERSION = 7a20c6771a9d038c35878a4d3904b965f2677c48
+SYSID101_SITE_METHOD = git
+SYSID101_SITE = ssh://git@github.com/flatmax/sysid101
+SYSID101_AUTORECONF = YES
+SYSID101_DEPENDENCIES += libtool
+
+
+$(eval $(autotools-package))
