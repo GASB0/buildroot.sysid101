1cd9e5dade10834634de013981e6b7a9e7c84782
Gabriel Santos <santos.baez.gabriel.elo@gmail.com>
2023-02-14 19:26:08 -0400

    adding the gtkiostream as dependency of sysid101

--- package/sysid101/sysid101.mk
+++ package/sysid101/sysid101.mk
@@ -8,7 +8,7 @@ SYSID101_VERSION = 7a20c6771a9d038c35878a4d3904b965f2677c48
 SYSID101_SITE_METHOD = git
 SYSID101_SITE = ssh://git@github.com/flatmax/sysid101
 SYSID101_AUTORECONF = YES
-SYSID101_DEPENDENCIES += libtool
+SYSID101_DEPENDENCIES += gtkiostream
 
 
 $(eval $(autotools-package))
